4699b31cc6de5da84f1f54923771cf393ded7c39
Ramkantodey <ramkantodey@gmail.com>
2024-09-06 15:28:03 +0600

    task one done

--- task-1.js
+++ task-1.js
@@ -1,3 +1,9 @@
 // Task-1
 // Take four parameters. Multiply the four numbers and then return the result
 
+function multiply(num1, num2, num3, num4){
+    return num1 * num2 * num3 * num4;
+}
+
+const result = multiply(2, 3, 4, 5);
+console.log(result);
